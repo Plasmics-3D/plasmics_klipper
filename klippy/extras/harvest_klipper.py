6877d8fbe1d1c58ea75544fee2e77cb06fe07afa
Johannes Zischg <joey@DESKTOP-2084V14.plasmics.local>
2024-01-23 12:56:12 +0100

    refacturing gcode_tracker into harvest_klipper, making it easier to store different information in individual batches

---
+++ klippy/extras/harvest_klipper.py
@@ -0,0 +1,201 @@
+import logging
+import os
+import importlib
+import threading
+import sys
+import subprocess
+import random
+
+# setting path
+sys.path.append("/home/pi/harvest")
+from shutdown_object import ShutdownObject
+
+OUTPUT_PATH = "/home/pi/harvest/output"
+
+
+class HarvestKlipper:
+    """Custom module used to collect information about gcode and other klipper-related information (refer to the diagram contained in the harvest repository for more information on this)."""
+
+    def __init__(self, config):
+        """Sets up the module and starts the harvest_main.py script (if possible)
+
+        :param config: contains the information from the config file for the 'harvest_klipper' section
+        :type config: ?
+        :raises ValueError: If the harvest folder is not available (i.e. the repository is not present or installed in a wrong location) an error is raised
+        - this should avoid useless collection of data with no way of matching it with other data.
+        """
+        logging.info("J: Harvest-klipper initiated!")
+        self.printer = config.get_printer()
+        self.reactor = self.printer.get_reactor()
+        self.gcode = self.printer.lookup_object("gcode")
+        self.motion_report = self.printer.lookup_object("motion_report")
+        self.virtual_sdcard = self.printer.lookup_object("virtual_sdcard")
+
+        _ = config.get("serial", "")
+        self.get_position_time_delta = config.getfloat("get_position_time_delta", 0.1)
+
+        self.gcode.register_output_handler(self._respond_raw)
+        self.printer.register_event_handler("klippy:shutdown", self._handle_shutdown)
+        self.printer.register_event_handler("klippy:disconnect", self._handle_shutdown)
+
+        if not os.path.exists("/home/pi/harvest"):
+            raise ValueError("harvest not installed - no data collection possible!")
+        self._create_output_folder()
+        self.print_job_id = "NO_ID_KLIPPER"
+        self.new_print_job_flag = False
+
+        self.batches = {
+            "gcode": {
+                "counter": 0,
+                "batch": [],
+                "batch_counter": [],
+                "last": None,
+                "file_path": os.path.join(
+                    OUTPUT_PATH,
+                    f"{self.print_job_id}_gcode_{0}.csv",
+                ),
+            },
+            "toolheadposition": {
+                "counter": 0,
+                "batch": [],
+                "batch_counter": [],
+                "last": None,
+                "file_path": os.path.join(
+                    OUTPUT_PATH,
+                    f"{self.print_job_id}_toolheadposition_{0}.csv",
+                ),
+            },
+        }
+
+        self.all_batch_names = list(self.batches.keys())
+
+        # start harvest
+        script_name = "/home/pi/harvest/harvest_main.py"
+        try:
+            with open("/home/pi/harvest/harvest.log", "a+") as log_file:
+                process = subprocess.Popen(
+                    ["/home/pi/harvest-env/bin/python", script_name],
+                    stdout=log_file,
+                    stderr=subprocess.STDOUT,
+                    shell=False,
+                )
+                logging.info(
+                    f"Started {script_name} in a new process. Process ID: {process.pid}"
+                )
+        except Exception as e:
+            logging.info(f"Error starting {script_name}: {e}")
+
+        # get the calculated position of the printer every get_position_time_delta ms
+        self.timer = self.reactor.register_timer(
+            self._get_printer_position, self.reactor.NOW
+        )
+
+    def get_status(self, eventtime) -> dict:
+        """This function is present in most modules and allows to read out the status of this module
+        over different queries. Not needed, but included for the sake of completness.
+
+        :param eventtime: The time of when the get_status was actually called
+        :type eventtime: ?
+        :return: Dictionary of the eventtime, and the name of this module (as a placeholder)
+        :rtype: dict
+        """
+        return {"eventtime": eventtime, "name": "Harves-klipper"}
+
+    def _respond_raw(self, msg: str) -> None:
+        """Script that is triggered if the gcode object sends out a message. If a file is done printing, the
+        batch information that is still in the buffer will be stored to a file.
+
+        :param msg: the message sent out by the gcode object
+        :type msg: str
+        """
+        # logging.info(f"J: Harvest-klipper respond raw triggered with {msg}")
+        if msg == "Done printing file":
+            logging.info(f"J: Harvest-klipper done - write last batch")
+            for i in self.all_batch_names:
+                self._write_batch_to_file(batch_name=i)
+
+    def _handle_shutdown(self) -> None:
+        """Shutdown function that writes batch information that is still in the buffer to a file"""
+        logging.info(f"J: Harvest-klipper handling shutdown - writing last batch")
+        for i in self.all_batch_names:
+            self._write_batch_to_file(batch_name=i)
+
+    def _print_start_processing(self, line):
+        if "SDCARD_PRINT_FILE" in line:
+            tmp = line.split("FILENAME=")[1].replace('"', "")
+            self.print_job_id = f"{tmp}_{self.reactor.monotonic()}_{random.randint(100000000,999999999)}"
+            self.new_print_job_flag = True
+        else:
+            self.new_print_job_flag = False
+
+    def _create_output_folder(self) -> None:
+        """If the destination for storing the batch information is not existing yet, creat the respective folder"""
+        if not os.path.exists(OUTPUT_PATH):
+            os.mkdir(os.path.join(OUTPUT_PATH))
+            logging.info(
+                "J: Harvest-klipper: output folder for data collection created"
+            )
+
+    def _write_batch_to_file(self, batch_name: str) -> None:
+        """Upon execution, write all the batch information currently present in the buffer to the output file and reset the batch
+
+        :param batch_name: the name of the batch that should be written to file
+        :type batch_name: str"""
+
+        current_batch = self.batches[batch_name]["batch"]
+        if len(current_batch["batch"]) > 0:
+            logging.info(f"J: Writing batch {batch_name} to file")
+            open(current_batch["file_path"], "a+").writelines(current_batch["batch"])
+            logging.info("J: Writing batch {batch_name} to file - done")
+            current_batch["batch"] = []
+            current_batch["batch_counter"] += 1
+            current_batch["file_path"] = os.path.join(
+                OUTPUT_PATH,
+                f"{self.print_job_id}_{batch_name}_{self.current_batch['batch_counter']}.csv",
+            )
+
+    def add_to_batch(self, batch_name: str, entry: str) -> None:
+        """Adds a new entry to the a batch with the current (event)time.
+        If a new print job is started, respectively a new print_job_id will will be passed, the last command and counter is resetted and the
+        path to the output file is adjusted accordingly
+
+        :param batch_name: the name of the batch were the entry should be added to
+        :type batch_name: str
+        :param entry: the entry element that should be stored
+        :type entry: str
+        """
+        self._print_start_processing(entry)
+        current_batch = self.batches[batch_name]["batch"]
+        if self.new_print_job_flag:
+            self._write_batch_to_file(batch_name=batch_name)
+            current_batch["last"] = ""
+            current_batch["counter"] = 0
+            current_batch["batch_counter"] = 0
+            current_batch["file_path"] = os.path.join(
+                OUTPUT_PATH,
+                f"{self.print_job_id}_{batch_name}_{0}.csv",
+            )
+        else:
+            self.gcode_last_cmd = entry
+            current_batch["counter"] += 1
+        current_batch["batch"].append(
+            f"{current_batch['counter']},{self.reactor.monotonic()},{entry}\n"
+        )
+        if len(current_batch["batch"]) >= 1000:
+            self._write_batch_to_file(batch_name=batch_name)
+
+    def _get_printer_position(self, eventtime):
+        if self.virtual_sdcard.is_active():
+            try:
+                # logging.info(
+                #     f"J: Harvest-klipper: timestamp: {self.reactor.monotonic()},{eventtime},{self.motion_report.get_status(eventtime)}"
+                # )
+                line = f"{eventtime},{self.motion_report.get_status(eventtime)}"
+                self.add_to_batch(batch_name="toolheadposition", entry=line)
+            except Exception as e:
+                logging.info(f"J: Harvest-klipper printer position ERROR: {e}")
+        return eventtime + self.get_position_time_delta
+
+
+def load_config(config):
+    return HarvestKlipper(config)
